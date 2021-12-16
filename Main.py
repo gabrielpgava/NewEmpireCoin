0413dc1f5c460e2eb6839a2bfef77ab236e4a74f
Gabriel P Gava <gabrielpasinigava@gmail.com>
2021-12-16 20:02:30 -0300

    AccountModel

--- Main.py
+++ Main.py
@@ -5,43 +5,16 @@ from TransactionPool import TransactionPool
 from Block import Block
 from Blockchain import Blockchain
 import pprint
-
+from AccountModel import AccountModel
 
 
 if __name__ == "__main__":
     
-    sender="sender"
-    receiver="receiver"
-    amount=1
-    type="TRANSFER"
     
     wallet = Wallet()
-    fraudulentWallet = Wallet()
-    blockchain = Blockchain()
-    pool = TransactionPool()
+    accountModel = AccountModel()
 
+    accountModel.updateBalace(wallet.publicKeyString(), 100)
+    accountModel.updateBalace(wallet.publicKeyString(), -10)
 
-    transaction = wallet.createTransaction(receiver,amount,type)
-
-    if pool.transactionExists(transaction) == False:
-        pool.addTransaction(transaction)
-    
-    lastHash = BlockchainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    blockCount = blockchain.blocks[-1].blockCount + 1
-    block = wallet.createBlock(pool.transactions, lastHash, blockCount)
-
-    if not blockchain.lastBlockHashValid(block):
-        print ('lastBlockHash is not valid')
-
-    if not blockchain.blockCountValid(block):
-        print ('blockCount is not valid')
-
-
-    if blockchain.lastBlockHashValid(block) and blockchain.blockCountValid(block):
-        blockchain.addBlock(block)
-
-
-    pprint.pprint(blockchain.toJson())
-
-
-    
+    pprint.pprint(accountModel.balances)
